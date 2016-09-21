1bd56999cd7b07d585dde2642bd782cb09e53dcc
marcelomelo <marcelo_e@estudante.sc.senai.br>
2016-09-21 17:09:09 +0000

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+    auth = request.env["omniauth.auth"]
+    session[:omniauth]= auth.except('extra')
+    user= User.sing_in_from_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_ulr, notice: "SIGNED IN"
+  end
+
+  def destroy
+     session [:user_id] = nil
+     session[:omniauth] = nil
+    redirect_to root_url, noticer: "SIGNED IN"
+  end
+  end
